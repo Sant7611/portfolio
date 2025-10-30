bfb2c2418259da65d5d56f19903098549c74d324
Santosh B <sbohara579@gmail.com>
2025-10-30 13:24:22 +0545

    some refactos

--- portfolio-site/src/components/Projects/ProjectCard.jsx
+++ portfolio-site/src/components/Projects/ProjectCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import avatar from "../../assets/avatar-prof.jpg";
 
 
-const ProjectCard = ({title, main}) => {
+const ProjectCard = ({title, main, source}) => {
   return (
     <div className='p-3 md:p-6 mb-5 flex flex-col md:w-70 md:h-auto w-64 h-80   bg-[#01010100] shadow-xl shadow-[#3f3b3b] rounded-2xl ' >
         <img src={avatar} alt="" className="p-4 w-full h-40 object-contain rounded-2xl" />
@@ -10,9 +10,8 @@ const ProjectCard = ({title, main}) => {
             {title}
         </h3>
         <p className="px-4 text-sm md:text-lg leading-tight py-2 ">{main}</p>
-        <div className="mt-2 p-2 md:p-4 flex gap-2 md:gap-4 ">
-            <button className='md:mt-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300  hover:scale-105 font-semibold rounded-3xl bg-primary ' >Demo</button>
-            <button className='md:mt-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300  hover:scale-105 font-semibold rounded-3xl bg-primary ' >Source Code</button>
+        <div className="mt-2 p-2 md:p-4 flex gap-2 md:gap-4 w-full">
+            <a href={source} className='md:mt-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300  hover:scale-105 font-semibold rounded-3xl bg-primary ' >Source Code</a>
         </div>
     </div>
   )
--- portfolio-site/src/components/Projects/Projects.jsx
+++ portfolio-site/src/components/Projects/Projects.jsx
@@ -6,19 +6,16 @@ const Projects = () => {
     {
       title: "News Magazine",
       main: "This is a news magazine website",
-      demo: "",
       source: "https://github.com/Sant7611/news",
     },
     {
       title: "News Magazine",
       main: "This is a news magazine website",
-      demo: "",
       source: "https://github.com/Sant7611/Project",
     },
     {
       title: "News Magazine",
       main: "This is a news magazine website",
-      demo: "",
       source: "https://github.com/Sant7611/portfolio/tree/main/portfolio-site",
     },
   ];
@@ -26,14 +23,10 @@ const Projects = () => {
   <section id="projects" className="scroll-mt-20 md:mx-20 p-10 md:p-12 text-white">
       <h1 className="text-2xl md:text-4xl text-white font-bold ">Projects</h1>
       <div className="py-12 px-8 flex flex-wrap gap-5 ">
-        <ProjectCard
-          title="Blogging Website"
-          main="This is a news magazine website"
-        />
-        <ProjectCard
-          title="Blogging Website"
-          main="This is the blogging website"
-        />
+        {projects.map((project, index)=>{
+          return <ProjectCard key={index} title={project.title} main={project.main} source={project.source} />
+        })}
+       
       </div>
     </section>
   );
