e08dd36e5e1848019f6c0cb6c30cb92b42e7b426
santosh bohara <108050563+Sant7611@users.noreply.github.com>
2025-11-07 20:02:55 +0545

    Revise Home component content and structure
    Updated the introduction text and changed the button to a link.

--- portfolio-site/src/components/Home/Home.jsx
+++ portfolio-site/src/components/Home/Home.jsx
@@ -4,24 +4,23 @@ import TextChange from "../TextChange";
 
 const Home = () => {
   return (
-  <div id="home" className="scroll-mt-20 text-white flex flex-col-reverse md:flex-row w-full  justify-between gap-7  p-10 md:p-20">
+  <section id="home" className="scroll-mt-20 text-white flex flex-col-reverse md:flex-row w-full  justify-between gap-7  p-10 md:p-20">
     
       <div className="md:w-2/4 md:pt-10">
         <h1 className="text-3xl md:text-4xl md:h-2/5 h-15 font-bold flex leading-normal ">
           <TextChange  />
         </h1>
         <p className="text-sm md:text-2xl tracking-tight ">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed iusto
-          dolor saepe
+          I am a motivated learner passionate about frontend development, eager to grow, collaborate effectively, and improve my skills through experience.
         </p>
-        <button className="mt-5 md:m-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 rounded-3xl bg-primary  ">
+        <a href="#contact" className="mt-5 n0-underline md:m-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 rounded-3xl bg-primary  ">
           Contact Me
-        </button>
+        </a>
       </div>
       <div className="flex justify-center items-center md:w-2/5">
         <img src={avatar} className=" w-2/5  md:w-72 rounded-full" alt="profile" />
       </div>
-    </div>
+    </section>
   );
 };
 
